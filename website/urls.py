706d0ea466f313f8a5c882459e805c63f45ce97b
edghimakoll <edghimakoll@gmail.com>
2021-12-23 15:15:22 +0100

    Ajout des templates base et portfolio_details, de l'url et de la vue

--- website/urls.py
+++ website/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from .views import PortfolioDetailView
 
 urlpatterns = [
     path('', views.index, name="home"),
-    
+    path('portfolio/<int:pk>/detail', PortfolioDetailView.as_view(), name="portfolio-detail"),
+
 ]
--- website/views.py
+++ website/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
-
+from django.views.generic import DetailView
 from website.models import Communautaire, Diplome, Experience, Item_portfolio, Profil, Skill
+
 
 
 def index(request):
     profil = Profil.objects.first()
     liste_skill_langage = Skill.objects.filter(categorie="Langage")
     liste_skill_techno = Skill.objects.filter(categorie="Technologie")
-    liste_experience = Experience.objects.all().order_by('id')
+    liste_experience = Experience.objects.all().order_by('-id')
     liste_diplome = Diplome.objects.all().order_by('-debut_annee')
-    liste_communautaire = Communautaire.objects.all()
+    liste_communautaire = Communautaire.objects.all().order_by('-id')
     liste_item_portfolio = Item_portfolio.objects.all()
     return render(request, 'website/index.html', {
         'profil': profil,
@@ -20,3 +21,8 @@ def index(request):
         'liste_communautaire': liste_communautaire,
         'liste_item_portfolio': liste_item_portfolio,
     })
+
+
+class PortfolioDetailView(DetailView):
+    model = Item_portfolio
+    template_name = 'portfolio_details.html'
